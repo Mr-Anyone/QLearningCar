Bug fix: Fixed vector calculation: radian to degree

Game/Car.py
@@ -10,7 +10,7 @@ class Car():
         self.vel = np.array([0, -self.speed]) # Moving 10 from each side
 
         self.current_degree = 0
-        self.angular_velocity = 1
+        self.angular_velocity = 5
 
     def move_front(self):
         """
@@ -40,7 +40,7 @@ class Car():
         """
 
         self.current_degree += self.angular_velocity
-        self.vel =  np.array([np.cos(self.current_degree) * self.speed, np.sin(self.current_degree) * self.speed])
+        self.vel =  np.array([np.cos(self.current_degree * np.pi/180) * self.speed, np.sin(self.current_degree* np.pi/180) * self.speed])
         self.pos += self.vel
 
     def change_default_value(self, vel=10, angular_velocity=5):
@@ -52,5 +52,5 @@ class Car():
         :return:
         """
         self.vel = vel
-        self.angular = angular_velocity
+        self.angular_velocity = angular_velocity
 

Game/Game.py
@@ -5,11 +5,11 @@ import numpy as np
 import pygame
 
 run = True # The state of the game
-car = Car(pos = np.array([0, 0]))
+car = Car(pos = np.array([WINDOWS_WIDTH/2, WINDOWS_HEIGHT/2]))
 
 pygame.init()
 clock = pygame.time.Clock() # The clock or the update speed of the game
-display = pygame.display.set_caption("Car Learns to drive")
+display = pygame.display.set_caption("AI Leans to drive")
 windows = pygame.display.set_mode((WINDOWS_WIDTH, WINDOWS_HEIGHT))
 FPS = 30
 
@@ -24,6 +24,13 @@ while run: # Game Loop
     if keys[pygame.K_w]:
         car.move_front()
 
+    if keys[pygame.K_a]:
+        car.turn_left()
+
+    if keys[pygame.K_s]:
+        car.move_backward()
+
+    print(car.current_degree)
     windows.fill((255,255,255)) # Drawing the background
     draw_car(car, windows)
 