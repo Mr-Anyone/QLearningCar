bedb2fb9eab1a6ab7edf4a7a3929062dd675ba4e
Vincent He <vincent0710@Outlook.com>
2020-10-31 22:25:58 -0400

    Added a colour and a track path

--- Game/Constants.py
+++ Game/Constants.py
@@ -1,3 +1,6 @@
+import os
+
+
 WINDOWS_WIDTH = 1000
 WINDOWS_HEIGHT = 800
 
@@ -6,3 +9,6 @@ BOX_HEIGHT = 20
 
 ORANGE = (255, 95, 43)
 RED = (255, 0, 44)
+BLUE = (0, 0, 255)
+
+TRACK_DIR = os.path.join(os.curdir, "Images", "Track.csv")
