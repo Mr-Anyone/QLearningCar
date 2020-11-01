610458ff45ad773931ab73a32bfc6c240dcd82c9
Vincent He <vincent0710@Outlook.com>
2020-10-31 21:35:38 -0400

    Added a function to draw the corners of the car

--- Game/GameUpdate.py
+++ Game/GameUpdate.py
@@ -1,11 +1,31 @@
+import os
 import pygame
 from Constants import *
-from PIL import Image
-import os
+
 car_image = pygame.image.load(os.path.join(os.curdir, "Images", "Car.png"))
+
+
 def draw_car(car, windows):
+    """
+    Draw an car and return the position of the rect
+    :param car: The car object to get the position
+    :param windows: the windows that the car will be drawn on
+    :return: the position of the four conner of the car
+    """
     center = car.pos[0], car.pos[1]
     new_img = pygame.transform.rotate(car_image, -car.current_degree)
-    windows.blit(new_img, new_img.get_rect(center=center))
-    # pygame.draw.rect(windows, (90, 0, 0), pygame.Rect(
-    #     (car.pos[0], car.pos[1], BOX_WIDTH, BOX_HEIGHT)))  # Rect format is (left, top, width, height)
+    rect = new_img.get_rect(center=center)
+    windows.blit(new_img, rect)
+
+
+def draw_cardot(car, windows):
+    """
+    Drawing all the corner of the dot
+    :param car:
+    :param windows:
+    :return:
+    """
+    points = car.get_four_side()
+    for point in points:
+        pygame.draw.rect(windows, ORANGE, (point[0], point[1], 5, 5)) # Drawing the four point of the car
+    pygame.draw.rect(windows, ORANGE, (car.center_point()[0], car.center_point()[1], 1, 1))  # RECT Format (left, top, width, height)
