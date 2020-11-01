f05d649893c194910c52ca7145a05db48eff01ec
Vincent He <vincent0710@Outlook.com>
2020-11-01 15:04:46 -0500

    Added a function to calculate the intersection point with the given 2 lines, and a distance function

--- Game/utils.py
+++ Game/utils.py
@@ -33,3 +33,23 @@ def read_csv():
 
         f.close()
     return outer, inner
+
+def interesction_point(p1, p2, p3, p4):
+    """
+    :return: the interesction point with the given points
+    """
+    x1, y1 = p1[0], p1[1]
+    x2, y2 = p2[0], p2[1]
+    x3, y3 = p3[0], p3[1]
+    x4, y4 = p4[0], p4[1]
+
+    uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
+    uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
+    return np.array([x1 + (uA * (x2-x1)), y1 + (uA * (y2-y1))])
+
+def distance(p1, p2):
+    """
+    The distance for 2 points
+    :return: the shortest distance between two points using a^2 + b^2 = c^2
+    """
+    return np.sqrt((p1[0]-p2[0])**2 + (p1[1] - p2[1])**2)
