2ff0342fadbc13e96d4d7d6f2bca334a4b706553
Vincent He <vincent0710@Outlook.com>
2020-10-31 22:51:52 -0400

    Drawn Track in the Game

--- Game/Game.py
+++ Game/Game.py
@@ -1,8 +1,9 @@
 from Car import Car
-from GameUpdate import draw_car, draw_cardot
+from GameUpdate import draw_car, draw_cardot, draw_tracks
 from Constants import *
 import numpy as np
 import pygame
+from utils import read_csv
 
 run = True # The state of the game
 car = Car(pos = np.array([WINDOWS_WIDTH/2, WINDOWS_HEIGHT/2]))
@@ -11,6 +12,7 @@ pygame.init()
 clock = pygame.time.Clock() # The clock or the update speed of the game
 display = pygame.display.set_caption("AI Leans to drive")
 windows = pygame.display.set_mode((WINDOWS_WIDTH, WINDOWS_HEIGHT))
+outer_track, inner_track = read_csv()
 FPS = 30
 
 while run: # Game Loop
@@ -36,6 +38,7 @@ while run: # Game Loop
     windows.fill((255,255,255)) # Drawing the background
     draw_car(car, windows)
     draw_cardot(car, windows)
+    draw_tracks(windows, inner_track, outer_track)
 
     pygame.display.update()
     pygame.display.flip()
