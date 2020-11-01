064a42e0856c2ab6334faecb2c806b26f60b06f8
Vincent He <vincent0710@Outlook.com>
2020-10-31 22:42:45 -0400

    Added a feature to read the track file and convert it to an array

--- Game/utils.py
+++ Game/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+from Constants import TRACK_DIR
+import csv
 
 
 def rotation(degree, vet):
@@ -13,3 +15,23 @@ def rotation(degree, vet):
                                 [np.sin(radian), np.cos(radian)]])
 
     return np.matmul(rotation_matrix, vet.T)
+
+def read_csv():
+    outer, inner = [], []
+    with open(TRACK_DIR, 'r') as f:
+        file = csv.reader(f)
+        for row in file:
+            if row[0] and row[-1]:
+                inner.append((row[0], row[1]))
+                outer.append((row[2], row[3]))
+
+            elif row[0] and not row[-1]:
+                inner.append((row[0], row[1]))
+
+            else:
+                outer.append((row[2], row[3]))
+
+        f.close()
+    return outer, inner
+outer, innter = read_csv()
+print("hi")
