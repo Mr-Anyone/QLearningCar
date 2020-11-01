1a495a4b3c665e9e42cf640f5e64f283cefb4875
Vincent He <vincent0710@Outlook.com>
2020-10-31 22:25:43 -0400

    First working version of track creator

--- Game/Track.py
+++ Game/Track.py
@@ -1,6 +1,62 @@
-import numpy
+import time
+import numpy as np
+import pygame
+import csv
+from Car import Car
+from Constants import *
+from GameUpdate import draw_tracks, draw_car
+
+pygame.init()
+pygame.display.set_caption("Track Creator")
+windows = pygame.display.set_mode((WINDOWS_WIDTH, WINDOWS_HEIGHT))
+
+run = True
+inner_tracks = []
+outer_tracks = []
+get_inner = True
+clock = pygame.time.Clock()
 
 
-class Track():
-    def __init__(self, track):
-        self.track = numpy.array(track)
+def save(inner, outer):
+    with open(TRACK_DIR, "w") as f:
+        file = csv.writer(f)
+
+        if len(inner) > len(outer):
+            for x in range(len(inner)):
+                try:
+                    file.writerow([inner[x][0], inner[x][1], outer[x][0], outer[x][1]])
+                except IndexError:
+                    file.writerow([inner[x][0], inner[x][1]])
+
+car = Car(pos=np.array([WINDOWS_WIDTH / 2, WINDOWS_HEIGHT / 2]))
+while run:
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            save(inner_tracks, outer_tracks)
+            run = False
+
+    if pygame.mouse.get_pressed()[0]:
+        if get_inner:
+            inner_tracks.append(pygame.mouse.get_pos())
+            time.sleep(0.1)
+        else:
+            outer_tracks.append(pygame.mouse.get_pos())
+            time.sleep(0.1)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_k]:
+        if get_inner:
+            get_inner = False
+            time.sleep(0.5)
+        else:
+            get_inner = True
+            time.sleep(0.5)
+
+    windows.fill((0, 0, 0))
+    draw_car(car, windows)
+    draw_tracks(windows, inner_tracks, outer_tracks)
+
+    pygame.display.flip()
+    pygame.display.update()
