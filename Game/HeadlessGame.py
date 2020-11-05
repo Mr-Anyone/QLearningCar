adea55ad6ecf28457d28200a216c542dbe9e9565
Vincent He <vincent0710@Outlook.com>
2020-11-05 09:43:14 -0500

    Complete the step member

--- Game/HeadlessGame.py
+++ Game/HeadlessGame.py
@@ -1,13 +1,18 @@
+import numpy as np
 from Car import Car
 from Constants import *
-import numpy as np
+from GameUpdate import get_data_from_sensor, check_all_collision
+from utils import read_csv
+
+outer_track, inner_track = read_csv() # Constant variable
+
 
 class Game:
     """
     The game class that will take in a car object
     """
-    car = Car(pos=np.array([WINDOWS_WIDTH/2, BOX_HEIGHT/2]))
-    original_car = Car(pos=np.array([WINDOWS_WIDTH/2, BOX_HEIGHT/2]))
+    car = Car(pos=np.array([WINDOWS_WIDTH / 2, BOX_HEIGHT / 2]))
+    original_car = Car(pos=np.array([WINDOWS_WIDTH / 2, BOX_HEIGHT / 2]))
 
     def reset(self):
         """
@@ -22,6 +27,18 @@ class Game:
         :param action:
         :return:
         """
+        if action == ACTION_FRONT:
+            self.car.move_front()
+        elif action == ACTION_LEFT:
+            self.car.turn_left()
+        elif action == ACTION_RIGHT:
+            self.car.turn_right()
+
+        state = get_data_from_sensor(self.car, "_", inner_track, outer_track, draw=False) # This is in the from (car, windows, inner_track, outer_track, draw)
+        done = check_all_collision(self.car, inner_track, outer_track)
+        reward = self.get_reward() # Finding a reward function
+
+        return state, reward, done
 
     def render(self):
         """
@@ -31,10 +48,9 @@ class Game:
         """
         pass
 
-
     def get_reward(self):
         """
 
         :return: reward
         """
-        pass
+        return "A Place Holder"
